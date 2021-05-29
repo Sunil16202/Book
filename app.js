a9b211a836db5fc7527b961fe6f408f0f972df43
Sunil Gupta <sunil.gupta@kore.com>
2021-05-29 19:27:56 +0530

    layout module

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+
+
+//Load config
+dotenv.config({path: './config/config.env'})
+
+//connecting to mongoDB
+connectDB()
+
+const app = express();
+
+
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+
+// Routes
+app.use('/', require('./routes/index'));
+
+
+const PORT = process.env.PORT || 9000
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening in ${process.env.NODE_ENV} on port ${PORT}`);
+
+})
